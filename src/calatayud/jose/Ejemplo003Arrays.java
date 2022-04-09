6d2a026fc356b84e4df17dc1885c697982583836
jcalat <jcalataruiz@hotmail.com>
2022-04-09 19:42:55 +0200

    conseguido ordenar un array de manera ascendente

--- src/calatayud/jose/Ejemplo003Arrays.java
+++ src/calatayud/jose/Ejemplo003Arrays.java
@@ -2,23 +2,32 @@ package calatayud.jose;
 
 public class Ejemplo003Arrays {
     public static void main(String[] args) {
-        int[] listaNotas= new int[] {3,6,1,7,8,9,5,3,4,6,5};
-        boolean n=false;
+        int[] listaNotas = new int[]{3, 6, 1, 7, 18, 9, 59, 56, 4, 62, 5};
+        boolean n = false;
+        int cont=0;
 
         do {
-            for (int i =0;i< listaNotas.length-1;i++){
-                System.out.println(listaNotas [i]);
-                if (listaNotas[i]>listaNotas[i+1]) {
-                int temporal=listaNotas[i];
-                listaNotas[i]=listaNotas[i+1];
-                listaNotas[i+1]=temporal;
-                }else{
-                    n=false;
+            n=false;
+            for (int i = 0; i < listaNotas.length - 1; i++) {
+
+                cont=0;
+                if (listaNotas[i] > listaNotas[i + 1]) {
+                    int temporal = listaNotas[i];
+                    listaNotas[i] = listaNotas[i + 1];
+                    listaNotas[i + 1] = temporal;
+                    n=true;
+
+                } else {
+
                 }
-                }
-//        for (int j = 0;j<listaNotas.length;j++ ) {
-//            System.out.println(listaNotas[j])11225;
-//        }
+
+            }
+
+
         } while (n);
+            for (int i = 0; i < listaNotas.length - 1; i++){
+                System.out.println(listaNotas[i]);
+
+            }
     }
 }
